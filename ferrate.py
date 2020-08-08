f0aece237d6f585741e278ef0338a8d90ec5663e
ferrumcccp <wushuzhen1975@icloud.com>
2020-08-09 01:04:32 +0800

    i

--- ferrate.py
+++ ferrate.py
@@ -1,22 +1,29 @@
-"""Global data"""
+"""
+Ferrate Bot [WIP]
+"""
 
 
 import queue
+import requests
+
+
+"""Global data"""
 
 
 username = None
 token = None
+room_name = "Oriental-Sakura"
+room_id = None
+
+
 """ Network
 """
 
 
-import requests
-
-import global_data
-
 def main_loop():
-    global_data.username = input("用户名：")
-    global_data.token = input("口令：")
+    global username, token
+    username = input("用户名：")
+    token = input("口令：")
 
 if __name__ == "__main__":
     main_loop()
