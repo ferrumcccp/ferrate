2c63f43ee688813bc07747d2e322db97b8620a35
ferrumcccp <wushuzhen1975@icloud.com>
2020-08-08 16:19:02 +0800

    update github login

--- github-login.py
+++ github-login.py
@@ -1,40 +1,39 @@
 from requests import session
 from bs4 import BeautifulSoup as bs
 
-USER = 'wushuzhen1975@icloud.com'
-PASSWORD = input("Type password:")
+user_mail = 'wushuzhen1975@icloud.com'
+username = "ferrumcccp"
+password = input("Type password:")
 
-URL1 = 'https://github.com/session'
-URL2 = 'https://github.com/'
+url_session = 'https://github.com/session'
+url_test = 'https://github.com/'
 
 
 with session() as s:
-    req = s.get(URL1).text
+    req = s.get(url_session).text
     html = bs(req, "lxml")
     token = html.find("input", {"name": "authenticity_token"}).attrs['value']
     com_val = html.find("input", {"name": "commit"}).attrs['value']
 
-    login_data = {'login': USER,
-        'password': PASSWORD,
+    login_data = {'login': user_mail,
+        'password': password,
         'commit' : com_val,
         'authenticity_token' : token}
 
     print("===1")
 
-    r1 = s.post(URL1, data = login_data)
+    r1 = s.post(url_session, data = login_data)
 
     print("===2")
 
-    r2 = s.get(URL2)
+    r2 = s.get(url_test)
     print("===3")
     data2 = r2.content
 
     page_html = data2
 
-    print(page_html)
-
-    page_soup = bs(page_html, "html.parser")
-
-    containers = page_soup.findAll("div", {"class":"mb-1"})
-    print("On this page, there are how many projects listed? \n")
-    print(len(containers))
+    if username in str(page_html):
+        print("Login successful.")
+    else:
+        print("Login failed.")
+        sys.exit(0)
